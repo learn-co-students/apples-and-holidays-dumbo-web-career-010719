1f7fe1c3b5f9892bcd9a641e917615e11143c9b2
Edwin Lin <edwinlin@gmail.com>
2018-12-11 12:19:32 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,12 +18,16 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  return holiday_hash.values[1].values.flatten[1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  holiday_hash[:winter].values.each do |ele|
+    ele << supply
+  end
 
 end
 
@@ -31,18 +35,21 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring].values.each do |ele|
+    ele << supply
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
+  holiday_hash[season][holiday_name] = supply_array
   # remember to return the updated hash
-
+  return holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  return holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,13 +60,28 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  
+  holiday_hash.keys.each do |key|
+    puts "#{key.to_s.capitalize!}:"
 
+    holiday_hash[key].each do |k,v|
+      puts "  #{(k[0..-1]).split("_").map{|word| word.to_s.capitalize!}.join(" ")}: #{v.join(", ") }"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  array = []
+  holiday_hash.each do |key, value|
+    holiday_hash[key].each do |k2, v2|
+      if v2.include?("BBQ")
+        array << k2
+      end
+    end
+  end
+  return array
 end
 
 
